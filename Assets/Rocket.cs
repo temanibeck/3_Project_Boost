0054e27d1e9fe3e8645ef4a25984caa8d3ad454d
Temani <temani@keysysconsulting.com>
2019-04-05 14:50:17 -0500

    Added audio clip to rocket launch

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,6 +27,15 @@ public class Rocket : MonoBehaviour
         {
             print("Thrusting...");
             rigidBody.AddRelativeForce(Vector3.up);
+
+            if(!audioSource.isPlaying)//so it doesn't layer
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
